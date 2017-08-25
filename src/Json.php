ab6feeb6c6df0e9ef4d2e932fde7b734174a3008
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2017-08-26 00:26:57 +0100

    Throw error on json error

--- src/Json.php
+++ src/Json.php
@@ -6,6 +6,11 @@ class Json
 {
     public function decode($json)
     {
-        return json_decode($json);
+        $result = json_decode($json);
+        if (json_last_error()) {
+            $error = sprintf('%s', json_last_error_msg());
+            throw new \Exception($error);
+        }
+        return $result;
     }
 }
